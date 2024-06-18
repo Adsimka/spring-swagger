7b618e888f980e76cdd2d62b013ce987d0237f5a
Arseniy <sahalysyk02@mail.ru>
2024-06-18 23:23:45 +0500

    Setting up a specification using the @OpenAPIDefinition annotation

--- src/main/java/com/swagger/SwaggerApplication.java
+++ src/main/java/com/swagger/SwaggerApplication.java
@@ -1,9 +1,28 @@
 package com.swagger;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(
+		info = @Info(
+			title = "UserOpenApi",
+			description = "demo project",
+			version = "1.0",
+			contact = @Contact(
+				name = "API Support",
+				url = "localhost:8080/support",
+				email = "sahalysyk02@mail.ru"
+			)
+		),
+		servers = {
+				@Server(url = "http/localhost:8095")
+		}
+)
 public class SwaggerApplication {
 
 	public static void main(String[] args) {
